4ca7c3a696e41b24a36e9e33a4d5786f1ac3edf5
forrgii <146547228+forrgii@users.noreply.github.com>
2023-09-30 21:56:18 +0000

    1 classes

---
+++ main.py
@@ -0,0 +1,25 @@
+import mesa
+import sage
+import simpy
+
+# fond companies[a1 a2 a3] agents[users] => applicates to buy actions
+# delete bonkrote company   
+
+class User(mesa.Agent):
+    def __init__(self):
+
+class company(mesa.Agnet):
+    def __init__(self):
+        int capital 
+
+##########################################
+
+class stock(mesa.Agent):
+    def __init__(self):
+
+##########################################
+class buying(mesa.Model):
+    def __init__(self):
+        self.user = User()
+        self.company = Company()
+        self.stock = Stock()
