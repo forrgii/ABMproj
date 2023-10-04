96a7c7536c6443f87615d489003b62850c67dc30
forrgii <nekolo1997@inbox.ru>
2023-10-04 16:13:34 +0300

    1.1

--- main.py
+++ main.py
@@ -1,29 +1,47 @@
+from typing import Any
 import mesa
+from mesa.model import Model
 import sage
 import simpy
 
 # fond companies[a1 a2 a3] agents[users] => applicates to buy actions
 # delete bonkrote company   
 
-class User(mesa.Agent):
-    def __init__(self):
-        print("I`m User")
-class Company(mesa.Agent):
-    def __init__(self):
-        capital = 0 
-        print("I`m Company")
-
-##########################################
+############### Subclasses ################
 
 class Stock(mesa.Agent):
-    def __init__(self):
-        print("I`m Stock")
+    def __init__(self, cost):
+        self.cost=cost
+        #print("I`m Stock")
 
-##########################################
-class buying(mesa.Model):
+###### Main classes ######################
+class User(mesa.Agent):
+    def __init__(self, unique_id, model):
+        super().__init__(unique_id, model)
+        self.money = 0
+    #def generate_request(Company, count):
+        
+
+class Company(mesa.Agent):
+    def __init__(self, capital, Stock, stock_count):
+        self.capital = capital
+        self.stock_cost = Stock.cost
+        self.stock_count = stock_count
+    def getInf(self):
+        return(self.capital, self.stock_cost, self.stock_count)
+
+
+######### Models and other functions ########
+
+class buying(mesa.Model, User, Company):
     def __init__(self):
         self.user = User()
         self.company = Company()
         self.stock = Stock()
 
-model = buying()
+############# test #######################
+
+a=Stock(13)
+b=Company(650, a, 13)
+print(*b.getInf())
+
