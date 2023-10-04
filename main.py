df004897460fa39e049c346d3b2bbdc02ce41e4f
forrgii <nekolo1997@inbox.ru>
2023-10-04 19:33:52 +0300

    v1.2

--- main.py
+++ main.py
@@ -7,41 +7,98 @@ import simpy
 # fond companies[a1 a2 a3] agents[users] => applicates to buy actions
 # delete bonkrote company   
 
-############### Subclasses ################
+############### Functions ################
 
-class Stock(mesa.Agent):
-    def __init__(self, cost):
-        self.cost=cost
-        #print("I`m Stock")
+#pair structure 
+def make_pair(x,y):
+    return lambda n: x if n==0 else y
+def first(p):
+    return p(0)
 
-###### Main classes ######################
-class User(mesa.Agent):
-    def __init__(self, unique_id, model):
-        super().__init__(unique_id, model)
-        self.money = 0
-    #def generate_request(Company, count):
-        
+def second(p):
+    return p(1)
+
+###### Main classes ######################      
 
 class Company(mesa.Agent):
+    class Stock():
+        def __init__(self, cost, name, id):
+            self.cost = cost
+            self.name = name
+            self.id = id
+
     def __init__(self, capital, Stock, stock_count):
+        
         self.capital = capital
         self.stock_cost = Stock.cost
         self.stock_count = stock_count
+
     def getInf(self):
         return(self.capital, self.stock_cost, self.stock_count)
 
+class User(mesa.Agent):
+    def __init__(self, unique_id, model) -> None:
+        super().__init__(unique_id, model)
+        self.money = 0
+        self.history = [] #name of company, one stock cost, count of stocks
+        self.account = []
+    def getHist(self):
+        return(self.history)
+    
+    #buying of stocks
+    def ReqBuy(self, count, Company):
+        if(self.money>Company.stock_cost * count and Company.stock_count <= count):
+            self.money -= Company.stock_cost * count
+            Company.stock_count -= count
+            Company.capital += Company.stock_cost * count
+            #name of company, one stock cost, count of stocks
+            self.offer=(Company.id, Company.stock_cost, count, "Bought")
+            self.history.push_back(self.offer)
+            #acc def
+            index=0
+            for i in self.account:
+                if(first(i)==Company.id):
+                    index=self.account.index(i)
+                    break
+            if (index):
+                self.account[index][1]+=count
+            else:
+                offer=make_pair(Company.id, count)
+                self.account.push_back(offer)
+        else:
+            print("wrong request")
 
-######### Models and other functions ########
+    #selling of stocks
+    def ReqSell(self, count, Company):
+        for i in self.account: 
+                if (first(i) == Company.id):
+                    if(Company.capital > Company.stock_cost * count and second(i)>=count):
+                        self.money += Company.stock_cost * count
+                        Company.stock_count += count
+                        Company.capital -= Company.stock_cost * count
+                        #name of company, one stock cost, count of stocks
+                        self.offer = (Company.id, Company.stock_cost, count, "Selled")
+                        self.history.push_back(self.offer)
+                        #acc def
+                        i[1] -= count
+                        if(second(i)==0):
+                            self.account.remove(i)
+                    else:
+                        print("wrong request")
+                    break
+
+        
+
+################# Models #################
 
 class buying(mesa.Model, User, Company):
     def __init__(self):
         self.user = User()
         self.company = Company()
-        self.stock = Stock()
 
 ############# test #######################
 
-a=Stock(13)
+a=Company.Stock(13)
 b=Company(650, a, 13)
 print(*b.getInf())
 
